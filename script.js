39e16c00b739ca417ca6cba0b079276baf091ca0
Carlos Henrique <fariascarlos2346@gmail.com>
2025-05-07 23:17:02 -0300

    fazendo imagens sumir

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const items = document.querySelectorAll('.item')
 const dots = document.querySelectorAll('.dot')
 const numberIndicator = document.querySelector('.numbers')
 const list = document.querySelector('.list')
+
+
+let active = 0;
+const total = items.length
+let time;
+
+
+function update(direction) {
+
+    document.querySelector('.item.active').classList.remove('active')
+    document.querySelector('.dot.active').classList.remove('active')
+
+}
+
+
+prevButton.addEventListener('click', () => {
+    update(-1)
+})
+
+nextButton.addEventListener('click', () => {
+    update(1)
+})
